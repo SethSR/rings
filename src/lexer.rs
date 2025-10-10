8094d2cb97f55e8de67e17546cee0154586b4dbe
srenshaw <srenshaw1@gmail.com>
2025-10-10 03:14:00 -0400

    Initial commit. We have a bunch of initial data specifications, and a basic Lexer.

---
+++ src/lexer.rs
@@ -0,0 +1,255 @@
+
+use std::ops::Range;
+
+use crate::{Data, TokenKind};
+
+pub fn eval(data: &mut Data) {
+	let Data {
+		source,
+		tok_list,
+		tok_pos,
+		identifiers,
+		..
+	} = data;
+	let mut lexer = Lexer { source, pos: 0 };
+	lexer.skip_whitespace_and_comments();
+	while lexer.next(tok_list, tok_pos, identifiers) {
+		lexer.skip_whitespace_and_comments();
+	}
+}
+
+#[derive(Default)]
+struct Lexer<'a> {
+	source: &'a str,
+	pos: usize
+}
+
+impl<'a> Lexer<'a> {
+	fn next(&mut self,
+		kinds: &mut Vec<TokenKind>,
+		starts: &mut Vec<usize>,
+		identifiers: &mut Vec<Range<usize>>,
+	) -> bool {
+		let start = self.pos;
+
+		let kind = match self.peek(0) {
+			None => {
+				kinds.push(TokenKind::Eof);
+				starts.push(start);
+				return false;
+			}
+
+			Some(c) if c.is_alphabetic() || c == '_' => {
+				self.advance();
+				while let Some(c) = self.peek(0) {
+					if !c.is_alphanumeric() && c != '_' {
+						break;
+					}
+					self.advance();
+				}
+				match &self.source[start..self.pos] {
+					"return" => TokenKind::Return,
+					"record" => TokenKind::Record,
+					"table" => TokenKind::Table,
+					"proc" => TokenKind::Proc,
+					"bool" => TokenKind::Bool,
+					"true" => TokenKind::True,
+					"false" => TokenKind::False,
+					"in" => TokenKind::In,
+					"if" => TokenKind::If,
+					"else" => TokenKind::Else,
+					"for" => TokenKind::For,
+					"u8" => TokenKind::U8,
+					"s8" => TokenKind::S8,
+					"u16" => TokenKind::U16,
+					"s16" => TokenKind::S16,
+					"u32" => TokenKind::U32,
+					"s32" => TokenKind::S32,
+
+					_ => {
+						// NOTE - srenshaw - We may want to intern/memoize the strings at some point if we end
+						// up storing tons of duplicates.
+						let ident_id = identifiers.len();
+						identifiers.push(start..self.pos);
+						TokenKind::Identifier(ident_id)
+					}
+				}
+			}
+
+			Some(c) if c.is_numeric() => {
+				self.advance();
+				while let Some(c) = self.peek(0) {
+					if !c.is_numeric() || c != '_' {
+						break;
+					}
+					self.advance();
+				}
+				let mut is_decimal = false;
+				if self.peek(0) == Some('.') {
+					is_decimal = true;
+					self.advance();
+					while let Some(c) = self.peek(0) {
+						if !c.is_numeric() || c != '_' {
+							break;
+						}
+						self.advance();
+					}
+				}
+				let text = &self.source[start..self.pos];
+				if is_decimal {
+					let num = text.replace('_', "")
+						.parse::<f64>()
+						.expect("unable to parse decimal");
+					TokenKind::Decimal(num)
+				} else {
+					let num = text.replace('_', "")
+						.parse::<i64>()
+						.expect("unable to parse integer");
+					TokenKind::Integer(num)
+				}
+			}
+
+			Some('-') => {
+				self.advance();
+				if self.expect('=') {
+					TokenKind::DashEqual
+				} else if self.expect('>') {
+					TokenKind::Arrow
+				} else {
+					TokenKind::Dash
+				}
+			}
+
+			Some(':') => {
+				self.advance();
+				if self.expect(':') {
+					TokenKind::ColonColon
+				} else if self.expect('=') {
+					TokenKind::ColonEqual
+				} else {
+					TokenKind::Colon
+				}
+			}
+
+			Some('+') => {
+				self.advance();
+				if self.expect('=') { TokenKind::PlusEqual } else { TokenKind::Plus }
+			}
+
+			Some('*') => {
+				self.advance();
+				if self.expect('=') { TokenKind::StarEqual } else { TokenKind::Star }
+			}
+
+			Some('/') => {
+				self.advance();
+				if self.expect('=') { TokenKind::SlashEqual } else { TokenKind::Slash }
+			}
+
+			Some('=') => {
+				self.advance();
+				if self.expect('=') { TokenKind::EqualEqual } else { TokenKind::Equal }
+			}
+
+			Some('!') => {
+				self.advance();
+				if self.expect('=') { TokenKind::BangEqual } else { TokenKind::Eof }
+			}
+
+			Some('<') => {
+				self.advance();
+				if self.expect('=') { TokenKind::LessEqual } else { TokenKind::Less }
+			}
+
+			Some('>') => {
+				self.advance();
+				if self.expect('=') { TokenKind::GreaterEqual } else { TokenKind::Greater }
+			}
+
+			Some('(') => { self.advance(); TokenKind::OParen }
+
+			Some(')') => { self.advance(); TokenKind::CParen }
+
+			Some('{') => { self.advance(); TokenKind::OBrace }
+
+			Some('}') => { self.advance(); TokenKind::CBrace }
+
+			Some('[') => { self.advance(); TokenKind::OBracket }
+
+			Some(']') => { self.advance(); TokenKind::CBracket }
+
+			Some(';') => { self.advance(); TokenKind::Semicolon }
+
+			Some(',') => { self.advance(); TokenKind::Comma }
+
+			Some('@') => { self.advance(); TokenKind::At }
+
+			Some(_) => { self.advance(); TokenKind::Eof }
+		};
+
+		kinds.push(kind);
+		starts.push(start);
+		true
+	}
+
+	fn skip_whitespace_and_comments(&mut self) {
+		loop {
+			match self.peek(0) {
+				Some(c) if c.is_whitespace() => {
+					self.advance();
+				}
+				Some('-') if self.peek(1) == Some('-') => {
+					self.advance();
+					self.advance();
+					while let Some(c) = self.peek(0) {
+						if c == '\n' {
+							break;
+						}
+						self.advance();
+					}
+				}
+				_ => break,
+			}
+		}
+	}
+
+	fn peek(&self, offset: usize) -> Option<char> {
+		self.source[self.pos..].chars().nth(offset)
+	}
+
+	fn expect(&mut self, ch: char) -> bool {
+		if self.peek(0) == Some(ch) {
+			self.advance();
+			true
+		} else {
+			false
+		}
+	}
+
+	fn advance(&mut self) {
+		if let Some(c) = self.peek(0) {
+			self.pos += c.len_utf8();
+		}
+	}
+}
+
+#[test]
+fn can_lex_a_simple_program() {
+	let source = "main { return 3; }";
+	let mut data = Data::new(source.into());
+	eval(&mut data);
+
+	assert_eq!(data.tok_list, [
+		TokenKind::Identifier(0),
+		TokenKind::OBrace,
+		TokenKind::Return,
+		TokenKind::Integer(3),
+		TokenKind::Semicolon,
+		TokenKind::CBrace,
+		TokenKind::Eof,
+	]);
+	assert_eq!(data.tok_pos, [ 0, 5, 7, 14, 15, 17, 18 ]);
+	assert_eq!(data.identifiers.len(), 1);
+	assert_eq!(data.identifiers[0], 0..4);
+}
+
---
+++ src/main.rs
@@ -0,0 +1,156 @@
+
+use std::{env, fmt, fs};
+use std::collections::{HashMap, VecDeque};
+use std::ops::Range;
+
+mod lexer;
+
+fn main() {
+	let mut args = env::args();
+	args.next();
+
+	let file_path = args.next()
+		.expect("expected source file");
+	let source = fs::read_to_string(file_path)
+		.expect("unable to read source file");
+
+	let data = compile(source);
+	println!("{data}")
+}
+
+type SrcPos  = usize;
+type TokenId = usize;
+type IdentId = usize;
+type GraphId = usize;
+type ScopeId = usize;
+type NodeId  = usize;
+
+type RowData = Vec<(IdentId, Range<usize>)>;
+
+pub fn compile(source: String) -> Data {
+	let mut data = Data::new(source);
+	lexer::eval(&mut data);
+	data
+}
+
+#[derive(Default)]
+pub struct Data {
+	source: String,
+	error: String,
+	/* Lexer */
+	tok_list: Vec<TokenKind>,
+	tok_pos: Vec<SrcPos>,
+	identifiers: Vec<Range<usize>>,
+	/* Discovery */
+	proc_start: Vec<TokenId>,
+	// val-name -> value
+	values: HashMap<IdentId, i64>,
+	// rec-name -> (field, type)*
+	records: HashMap<IdentId, RowData>,
+	tables: HashMap<IdentId, TableData>,
+	/* Parsing */
+	proc_queue: VecDeque<Task>,
+	node_graphs: Vec<NodeGraph>,
+	scope_stacks: Vec<ScopeStack>,
+	// procedures ready to be inlined and/or lowered
+	ready_procs: HashMap<IdentId, NodeGraph>,
+}
+
+impl Data {
+	pub fn new(source: String) -> Self {
+		Self {
+			source,
+			..Default::default()
+		}
+	}
+}
+
+impl fmt::Display for Data {
+	fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+		writeln!(f, "=== Rings Compiler ===")?;
+		writeln!(f)?;
+		write!(f, "Tokens:")?;
+		for (token, start) in self.tok_list.iter().zip(self.tok_pos.iter()) {
+			write!(f, " {token:?}[{start}]")?;
+		}
+		writeln!(f)?;
+		write!(f, "Identifiers:")?;
+		for (ident_id, identifier) in self.identifiers.iter().enumerate() {
+			write!(f, " [{ident_id}] {}", &self.source[identifier.clone()])?;
+		}
+		writeln!(f)
+	}
+}
+
+struct TableData {
+	row_count: u32,
+	memory_region: IdentId,
+	row_spec: RowData,
+}
+
+struct Task {
+	proc_name: IdentId,
+	tok_pos: SrcPos,
+	graph_id: GraphId,
+	scope_id: ScopeId,
+}
+
+struct NodeGraph {
+	node_kinds: Vec<NodeKind>,
+	use_defs: Vec<Vec<NodeId>>,
+	def_uses: Vec<Vec<NodeId>>,
+}
+
+struct ScopeStack(Vec<HashMap<IdentId, NodeId>>);
+
+enum TypeDef {
+	Int(u8),
+	Bool,
+}
+
+#[derive(Debug, PartialEq)]
+enum TokenKind {
+	Identifier(IdentId),
+	Integer(i64),
+	Decimal(f64),
+
+	// TODO - srenshaw - Add Fixed-Point identifiers
+
+	// FixedF(u8),
+	// FixedD(u8),
+
+	Record,
+	Table,
+	Proc,
+	Return,
+	Bool,
+	True,
+	False,
+	U8, S8,
+	U16, S16,
+	U32, S32,
+	In,
+	If,
+	Else,
+	For,
+	Arrow,
+	At,
+	Semicolon,
+	Comma,
+	Colon, ColonColon, ColonEqual,
+	OParen, CParen,
+	OBrace, CBrace,
+	OBracket, CBracket,
+	Plus,    PlusEqual,
+	Dash,    DashEqual,
+	Star,    StarEqual,
+	Slash,   SlashEqual,
+	Less,    LessEqual,
+	Greater, GreaterEqual,
+	Equal,   EqualEqual,
+	BangEqual,
+	Eof,
+}
+
+enum NodeKind {}
+
