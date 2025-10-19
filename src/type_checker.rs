0ca0e8b69c97d5f013f82e993680f28b7507f9ce
srenshaw <srenshaw1@gmail.com>
2025-10-19 19:33:51 -0400

    Pulled out some type-lattice code into it's own function 'meet_rings'. Moved the main type-checking code into the 'check_stmt' method, so it can be called by soon-to-be-added methods (check_block, check_if, etc).

--- src/type_checker.rs
+++ src/type_checker.rs
@@ -34,117 +34,110 @@ pub fn eval(data: &mut Data) {
 			print!("[{offset:3}][{:3}..{:3}] ", range.start, range.end);
 
 			let ast_id = proc_range.start + offset;
-			match node {
-				Kind::Int(num) => {
-					println!("AST-Int({num})");
-					checker.ast_to_type.insert(ast_id, Type::Top);
-				}
-
-				Kind::Dec(num) => {
-					println!("AST-Dec({num})");
-					checker.ast_to_type.insert(ast_id, Type::Top);
-				}
-
-				Kind::Ident(ident_id) => {
-					checker.check_ident(data, ident_id, ast_id);
-				}
-
-				Kind::Define(ident_id, var_type, ast_id) => {
-					if let Some(err_msg) = checker.check_define(data, ident_id, *ast_id, *var_type) {
-						println!("{checker:?}");
-						error::error(data, &err_msg, range.start);
-						return;
-					}
-				}
-
-				Kind::Assign(ident_id, ast_id) => {
-					if let Some(err_msg) = checker.check_assign(data, ident_id, *ast_id) {
-						println!("{checker:?}");
-						error::error(data, &err_msg, range.start);
-						return;
-					}
-				}
-
-				Kind::BinOp(op, left, right) => {
-					println!("AST-BinOp({} {op} {})", left.index(), right.index());
-					if let Some(err_msg) = checker.check_binop(data, ast_id, *op, left, right) {
-						error::error(data, &err_msg, range.start);
-						return;
-					}
-				}
-
-				Kind::UnOp(op, right) => {
-					println!("AST-UnOp({op}{})", right.index());
-				}
-
-				Kind::Return(Some(ast_id)) => {
-					println!("AST-Return({})", ast_id.index());
-					// let kind = &data.ast_nodes[*ast_id];
-					// let ret_type = synthesize(data, kind);
-					// if proc_type.ret_type != ret_type {
-					// 	error::error(data,
-					// 		&format!("TC - Expected return type {:?}, found '{ret_type:?}'",
-					// 			proc_type.ret_type,
-					// 		),
-					// 		proc_start + ast_offset,
-					// 	);
-					// 	return;
-					// }
-				}
-
-				Kind::Return(None) => {
-					println!("AST-Return(-)");
-					// if proc_type.ret_type != crate::Type::Unit {
-					// 	error::error(data,
-					// 		&format!("TC - no return value for procedure with '{:?}' return type",
-					// 			proc_type.ret_type),
-					// 		proc_start + ast_offset,
-					// 	);
-					// 	return;
-					// }
-				}
-
-				Kind::If(cond, then_block, else_block) => {
-					println!("AST-If({} -> {} <> {})", cond.index(), then_block.0.len(), else_block.0.len());
-				}
-
-				Kind::While(cond, block) => {
-					println!("AST-While({} -> {})", cond.index(), block.0.len());
-				}
-
-				Kind::For(vars, Some(table_id), Some(range), block) => {
-					println!("AST-For({vars:?} in {}[{}] -> {})", data.text(*table_id), range, block.0.len());
-				}
-
-				Kind::For(vars, Some(table_id), None, block) => {
-					println!("AST-For({vars:?} in {} -> {})", data.text(*table_id), block.0.len());
-				}
-
-				Kind::For(vars, None, Some(range), block) => {
-					println!("AST-For({vars:?} in [{}] -> {})", range, block.0.len());
-				}
-
-				Kind::For(vars, None, None, block) => {
-					println!("AST-For({vars:?} -> {})", block.0.len());
-				}
-
-				Kind::Block(block) => {
-					println!("AST-Block({})", block.0.len());
-				}
+			if let Some(err_msg) = checker.check_stmt(data, node, ast_id, proc_type.ret_type) {
+				println!("{checker:?}");
+				error::error(data, &err_msg, range.start);
+				return;
 			}
 		}
 	}
 }
 
 impl Checker {
-	fn check_ident(&mut self, data: &Data, ident_id: &identifier::Id, ast_id: ast::Id) {
+	fn check_stmt(&mut self, data: &Data,
+		node: &ast::Kind, ast_id: ast::Id, ret_type: crate::Type,
+	) -> Option<String> {
+		match node {
+			Kind::Int(num) => {
+				println!("AST-Int({num})");
+				self.ast_to_type.insert(ast_id, Type::Top);
+				None
+			}
+
+			Kind::Dec(num) => {
+				println!("AST-Dec({num})");
+				self.ast_to_type.insert(ast_id, Type::Top);
+				None
+			}
+
+			Kind::Ident(ident_id) => {
+				self.check_ident(data, ident_id, ast_id);
+				None
+			}
+
+			Kind::Define(ident_id, var_type, expr_id) => {
+				self.check_define(data, ident_id, *expr_id, *var_type)
+			}
+
+			Kind::Assign(ident_id, expr_id) => {
+				self.check_assign(data, ident_id, *expr_id)
+			}
+
+			Kind::BinOp(op, left, right) => {
+				println!("AST-BinOp({} {op} {})", left.index(), right.index());
+				self.check_binop(data, ast_id, *op, left, right)
+			}
+
+			Kind::UnOp(op, right) => {
+				println!("AST-UnOp({op}{})", right.index());
+				todo!()
+			}
+
+			Kind::Return(expr_id) => {
+				println!("AST-Return({})", expr_id
+					.map(|id| id.index().to_string())
+					.unwrap_or("-".to_string()));
+				self.check_return(*expr_id, ret_type)
+			}
+
+			Kind::Block(block) => {
+				println!("AST-Block({})", block.0.len());
+				todo!()
+			}
+
+			Kind::If(cond, then_block, else_block) => {
+				println!("AST-If({} -> {} <> {})", cond.index(), then_block.0.len(), else_block.0.len());
+				todo!()
+			}
+
+			Kind::While(cond, block) => {
+				println!("AST-While({} -> {})", cond.index(), block.0.len());
+				todo!()
+			}
+
+			Kind::For(vars, Some(table_id), Some(range), block) => {
+				println!("AST-For({vars:?} in {}[{}] -> {})", data.text(*table_id), range, block.0.len());
+				todo!()
+			}
+
+			Kind::For(vars, Some(table_id), None, block) => {
+				println!("AST-For({vars:?} in {} -> {})", data.text(*table_id), block.0.len());
+				todo!()
+			}
+
+			Kind::For(vars, None, Some(range), block) => {
+				println!("AST-For({vars:?} in [{}] -> {})", range, block.0.len());
+				todo!()
+			}
+
+			Kind::For(vars, None, None, block) => {
+				println!("AST-For({vars:?} -> {})", block.0.len());
+				todo!()
+			}
+		}
+	}
+
+	fn check_ident(&mut self, data: &Data,
+		ident_id: &identifier::Id, ast_id: ast::Id,
+	) {
 		println!("AST-Ident({})", data.text(*ident_id));
 		let new_type = self.ident_to_type.get(ident_id)
 			.unwrap_or(&Type::Top);
 		self.ast_to_type.insert(ast_id, *new_type);
 	}
 
-	fn check_define(&mut self, data: &Data, ident_id: &identifier::Id, ast_id: ast::Id,
+	fn check_define(&mut self, data: &Data,
+		ident_id: &identifier::Id, ast_id: ast::Id,
 		var_type: crate::Type,
 	) -> Option<String> {
 		println!("AST-Define({} : {var_type:?} = {})", data.text(*ident_id), ast_id.index());
@@ -201,12 +194,10 @@ impl Checker {
 			let left_type = self.ast_to_type[left];
 			let right_type = self.ast_to_type[right];
 			match left_type.meet(right_type) {
-				Type::Bot => {
-					Some(format!("TC - unable to apply '{op}' to types '{}' and '{}'",
-						left_type.display(data),
-						right_type.display(data),
-					))
-				}
+				Type::Bot => Some(format!("TC - unable to apply '{op}' to types '{}' and '{}'",
+					left_type.display(data),
+					right_type.display(data),
+				)),
 				new_type => {
 					// Types are able to meet
 					self.ast_to_type.insert(ast_id, new_type);
@@ -216,6 +207,22 @@ impl Checker {
 		} else {
 			todo!("add the rest of the binary operators")
 		}
+	}
+
+	fn check_return(&mut self,
+		ast_id: Option<ast::Id>, proc_type: crate::Type,
+	) -> Option<String> {
+		let ret_type = match ast_id {
+			Some(ast_id) => match self.ast_to_type.get(&ast_id) {
+				Some(ret_type) => *ret_type,
+				None => return Some(format!("CE - expression has no type: {ast_id:?}")),
+			}
+			None => Type::Rings(crate::Type::Unit),
+		};
+		if ret_type.meet(Type::Rings(proc_type)) == Type::Bot {
+			return Some(format!("TC - Expected return type {proc_type:?}, found '{ret_type:?}'"));
+		}
+		None
 	}
 }
 
@@ -238,55 +245,46 @@ impl Type {
 
 impl Type {
 	fn meet(self, rhs: Type) -> Type {
-		use crate::Type as Rings;
-
 		match (self, rhs) {
 			(Self::Top, _) => rhs,
 			(_, Self::Top) => self,
 			(Self::Bot, _) | (_, Self::Bot) => Self::Bot,
 
-			(Self::Rings(Rings::Record(a)), Self::Rings(Rings::Record(b))) => if a == b {
-				self
-			} else {
-				Self::Bot
-			}
-			(Self::Rings(Rings::Record(_)), _) | (_, Self::Rings(Rings::Record(_))) => Self::Bot,
-
-			(Self::Rings(Rings::Table(a)), Self::Rings(Rings::Table(b))) => if a == b {
-				self
-			} else {
-				Self::Bot
-			}
-			(Self::Rings(Rings::Table(_)), _) | (_, Self::Rings(Rings::Table(_))) => Self::Bot,
-
-			(Self::Rings(Rings::Unit), Self::Rings(Rings::Unit)) => Self::Rings(Rings::Unit),
-			(Self::Rings(Rings::Unit), _) | (_, Self::Rings(Rings::Unit)) => Self::Bot,
-
-			(Self::Rings(Rings::U32), Self::Rings(Rings::S32)) => Self::Bot,
-			(Self::Rings(Rings::S32), Self::Rings(Rings::U32)) => Self::Bot,
-
-			(Self::Rings(Rings::S16), Self::Rings(Rings::U32)) => Self::Bot,
-			(Self::Rings(Rings::S8), Self::Rings(Rings::U32)) => Self::Bot,
-			(Self::Rings(Rings::U32), Self::Rings(Rings::S16)) => Self::Bot,
-			(Self::Rings(Rings::U32), Self::Rings(Rings::S8)) => Self::Bot,
-
-			(Self::Rings(Rings::U16), Self::Rings(Rings::S16)) => Self::Bot,
-			(Self::Rings(Rings::S16), Self::Rings(Rings::U16)) => Self::Bot,
-
-			(Self::Rings(Rings::S8), Self::Rings(Rings::U16)) => Self::Bot,
-			(Self::Rings(Rings::U16), Self::Rings(Rings::S8)) => Self::Bot,
-
-			(Self::Rings(Rings::S8), Self::Rings(Rings::U8)) => Self::Bot,
-			(Self::Rings(Rings::U8), Self::Rings(Rings::S8)) => Self::Bot,
-
-			(Self::Rings(Rings::Bool), _) | (_, Self::Rings(Rings::Bool)) => Self::Rings(Rings::Bool),
-			(Self::Rings(Rings::S32), _) | (_, Self::Rings(Rings::S32)) => Self::Rings(Rings::S32),
-			(Self::Rings(Rings::U32), _) | (_, Self::Rings(Rings::U32)) => Self::Rings(Rings::U32),
-			(Self::Rings(Rings::S16), _) | (_, Self::Rings(Rings::S16)) => Self::Rings(Rings::S16),
-			(Self::Rings(Rings::U16), _) | (_, Self::Rings(Rings::U16)) => Self::Rings(Rings::U16),
-			(Self::Rings(Rings::S8), Self::Rings(Rings::S8)) => Self::Rings(Rings::S8),
-			(Self::Rings(Rings::U8), Self::Rings(Rings::U8)) => Self::Rings(Rings::U8),
+			(Self::Rings(a), Self::Rings(b)) => meet_rings(a, b)
+				.map(Self::Rings)
+				.unwrap_or(Self::Bot),
 		}
+	}
+}
+
+fn meet_rings(a: crate::Type, b: crate::Type) -> Option<crate::Type> {
+	use crate::Type as T;
+
+	match (a, b) {
+		(type_a, type_b) if type_a == type_b => Some(type_a),
+
+		(T::Record(_), _) | (_, T::Record(_)) => None,
+
+		(T::Table(_), _) | (_, T::Table(_)) => None,
+
+		(T::Unit, _) | (_, T::Unit) => None,
+
+		(T::U32, T::S32) | (T::S32, T::U32) => None,
+		(T::U32, T::S16) | (T::S16, T::U32) => None,
+		(T::U32, T::S8 ) | (T::S8 , T::U32) => None,
+
+		(T::U16, T::S16) | (T::S16, T::U16) => None,
+		(T::U16, T::S8)  | (T::S8 , T::U16) => None,
+		(T::U8 , T::S8)  | (T::S8 , T::U8 ) => None,
+
+		(T::S8, T::S8) => Some(T::S8),
+		(T::U8, T::U8) => Some(T::U8),
+
+		(T::Bool, _) | (_, T::Bool) => Some(T::Bool),
+		(T::S32, _) | (_, T::S32) => Some(T::S32),
+		(T::U32, _) | (_, T::U32) => Some(T::U32),
+		(T::S16, _) | (_, T::S16) => Some(T::S16),
+		(T::U16, _) | (_, T::U16) => Some(T::U16),
 	}
 }
 
